575b48b2b124daf03938464cc228aa97c9fcf5f7
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-08-28 12:05:11 -0700

    push 2

--- api/Controllers/UserController.cs
+++ api/Controllers/UserController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models.DTO;
+using api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -10,6 +12,18 @@ namespace api.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        
+        private readonly UserService _data;
+
+        public UserController(UserService dataFromService)
+        {
+            _data = dataFromService;
+        }
+
+        //add a user
+        public bool AddUser(CreateAccountDTO UserToAdd)
+        {
+            //create a variable with a type of service
+            return _data.AddUser(UserToAdd);
+        }
     }
 }
